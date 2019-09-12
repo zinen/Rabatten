9f42bbdf3a586dbc97ff23797fa23af313dc99b5
zinen <zinen@users.noreply.github.com>
2019-09-12 15:52:35 +0200

    Database moved to as .json Instead of .js

--- common.js
+++ common.js
@@ -8,13 +8,13 @@ const DiscountServices = {
   "forbrugsforeningen": [
     {
       "arrayName": "array_forbrugsforeningen",
-      "databaseURL": "https://cdn.jsdelivr.net/gh/zinen/Rabatten@latest/forbrugsforeningen.js",
+      "databaseURL": "https://cdn.jsdelivr.net/gh/zinen/Rabatten@latest/forbrugsforeningen.json",
       "homepage": "http://forbrugsforeningen.dk"
     },
   ], "logbuy": [
     {
       "arrayName": "array_logbuy",
-      "databaseURL": "https://cdn.jsdelivr.net/gh/zinen/Rabatten@latest/logbuy.js",
+      "databaseURL": "https://cdn.jsdelivr.net/gh/zinen/Rabatten@latest/logbuy.json",
       "homepage": "http://logbuy.com"
     },
   ],
