f9d6e4e62ec61836c2b86229e6ae503fcd0387b7
zinen <zinen@users.noreply.github.com>
2019-11-17 21:35:26 +0100

    Readable improvement

--- src/content.js
+++ src/content.js
@@ -50,14 +50,14 @@ chrome.storage.sync.get('memberships', function (membershipsarray) {
     if (matches.length > 0) {
       // Notify the background script of a match
       chrome.runtime.sendMessage({ rabatmatch: true, matchHolder: matches })
-      makeTopPane(matches)
+      handelMatches(matches)
     }
   }, function (err) {
     console.error(err)
   })
 })
 
-function makeTopPane (matchHolder) {
+function handelMatches (matchHolder) {
   if (!ignoredomain.includes(tabdomainname)) {
     debuglog('Content: matchholder info used')
     let text
@@ -77,28 +77,33 @@ function makeTopPane (matchHolder) {
       const service = '<a href=" //' + link + '"><font style="">' + matchHolder[0][4] + '</font></a>'
       text = shop + ' har ' + discount + ' igennem ' + service
     }
-    const newdiv = document.createElement('div')
-    newdiv.style = 'all:initial;position:absolute;left:0;top:0;'
-    newdiv.innerHTML =
-      '<div id="aso12909" style="width:100%;padding-right:10px;padding-left:10px;background-color:rgba(244,230,155,0.8);position: fixed;box-shadow: 0 2px 6px #3C4A54;z-index:9999999;top:0px">\n' +
-      '<font style="font-size:20px;font-family:Arial;">' +
-      '<img src="' + chrome.runtime.getURL('icon48.png') + '" alt="" style="margin-left:0px; margin-top:0px; margin-right:4px; width:28px; height:28px; vertical-align:sub;display:inline-block" />' +
-      text + '<button id="aso12910" style="font-size:20px;font-family:verdana;height: 28px;float: right;margin-right: 12px;border: none;background: none;cursor: pointer;">X</button>' +
-      '</font>\n' +
-      '</div>\n'
-    // Append div as the first child of body
-    const shadowdiv = document.createElement('div')
-    document.body.insertBefore(shadowdiv, document.body.firstChild)
-    const shadow = shadowdiv.attachShadow({ mode: 'open' })
-    shadow.appendChild(newdiv)
-    // Make close button function
-    const elem = shadow.getElementById('aso12910')
-    elem.addEventListener('click', function () {
-      elem.parentNode.parentNode.parentNode.remove()
-      chrome.storage.local.set({ rabat_closed: tabdomainname }, function () {
-        debuglog('Content: Close button domain name: ' + tabdomainname)
-      })
-    })
+    makeTopPane(text)
   }
 }
+
+function makeTopPane (text) {
+  const newdiv = document.createElement('div')
+  newdiv.style = 'all:initial;position:absolute;left:0;top:0;'
+  newdiv.innerHTML =
+    '<div id="aso12909" style="width:100%;padding-right:10px;padding-left:10px;background-color:rgba(244,230,155,0.8);position: fixed;box-shadow: 0 2px 6px #3C4A54;z-index:9999999;top:0px">\n' +
+    '<font style="font-size:20px;font-family:Arial;">' +
+    '<img src="' + chrome.runtime.getURL('icon48.png') + '" alt="" style="margin-left:0px; margin-top:0px; margin-right:4px; width:28px; height:28px; vertical-align:sub;display:inline-block" />' +
+    text + '<button id="aso12910" style="font-size:20px;font-family:verdana;height: 28px;float: right;margin-right: 12px;border: none;background: none;cursor: pointer;">X</button>' +
+    '</font>\n' +
+    '</div>\n'
+  // Append div as the first child of body
+  const shadowdiv = document.createElement('div')
+  document.body.insertBefore(shadowdiv, document.body.firstChild)
+  const shadow = shadowdiv.attachShadow({ mode: 'open' })
+  shadow.appendChild(newdiv)
+  // Make close button function
+  const elem = shadow.getElementById('aso12910')
+  elem.addEventListener('click', function () {
+    elem.parentNode.parentNode.parentNode.remove()
+    chrome.storage.local.set({ rabat_closed: tabdomainname }, function () {
+      debuglog('Content: Close button domain name: ' + tabdomainname)
+    })
+  })
+}
+
 debuglog('Content: script end.')
--- src/popup.js
+++ src/popup.js
@@ -13,47 +13,6 @@ document.addEventListener('DOMContentLoaded', function () {
   })
 }, false)
 
-function popuplatePopup (shop, discount, discountlink, service) {
-  const matchlist = document.getElementById('match-list')
-  const newEntry = document.createElement('li')
-  matchlist.appendChild(newEntry)
-  const title = document.createElement('p')
-  title.classList.add('title2')
-  title.innerText = shop
-  newEntry.appendChild(title)
-  let badge
-  if (service) {
-    badge = document.createElement('span')
-    badge.classList.add('badge')
-    badge.classList.add('right')
-    badge.innerText = service
-    newEntry.appendChild(badge)
-  }
-  const content = document.createElement('p')
-  content.classList.add('title3')
-  content.innerText = discount
-  newEntry.appendChild(content)
-  if (discountlink) {
-    const newA = document.createElement('a')
-    newA.classList.add('nounderline')
-    newA.appendChild(title)
-    if (badge) { newA.appendChild(badge) }
-    newA.appendChild(content)
-    newEntry.appendChild(newA)
-    newA.setAttribute('href', 'https://' + discountlink)
-    newA.setAttribute('title', 'Open link')
-    newEntry.classList.add('is-link')
-    // Add lister to redirect tab page on click in popup window
-    newEntry.addEventListener('mouseup', function (e) {
-      // Only update url if left mouse button is pressed. This means that middel click
-      // will open the url in a new tab, and do a url update
-      if (e.which === 1) {
-        chrome.tabs.update({ url: 'https://' + discountlink })
-      }
-    })
-  }
-}
-
 function fillPopup (content) {
   debuglog('Populates popup now')
   if (Array.isArray(content)) {
@@ -63,6 +22,54 @@ function fillPopup (content) {
   } else {
     popuplatePopup('No match on this site', 'Please report if this is wrong')
   }
+}
+
+function popuplatePopup (inTitel, inSubtitel, inLink, inBadge) {
+  const matchlist = document.getElementById('match-list')
+  const newEntry = document.createElement('li')
+  matchlist.appendChild(newEntry)
+  const title = document.createElement('p')
+  title.classList.add('title2')
+  title.innerText = inTitel
+  newEntry.appendChild(title)
+  const badge = makeBadge(inBadge)
+  if (inBadge) { newEntry.appendChild(badge) }
+  const content = document.createElement('p')
+  content.classList.add('title3')
+  content.innerText = inSubtitel
+  newEntry.appendChild(content)
+  if (inLink) { makeLink(title, badge, content, newEntry, inLink) }
+}
+
+function makeBadge (text) {
+  // Make a badge element, with input as its text
+  const badge = document.createElement('span')
+  badge.classList.add('badge')
+  badge.classList.add('right')
+  badge.innerText = text
+  return badge
+}
+
+function makeLink (title, badge, content, newEntry, inLink) {
+  // Transforms list obejct to a link
+  const newA = document.createElement('a')
+  newA.classList.add('nounderline')
+  // Append all underlying elements to new link object
+  newA.appendChild(title)
+  if (badge) { newA.appendChild(badge) }
+  newA.appendChild(content)
+  newEntry.appendChild(newA)
+  newA.setAttribute('href', 'https://' + inLink)
+  newA.setAttribute('title', 'Open link')
+  newEntry.classList.add('is-link')
+  // Add lister to redirect tab page on click in popup window
+  newEntry.addEventListener('mouseup', function (e) {
+    // Only update url if left mouse button is pressed. This means that middel click
+    // will open the url in a new tab, and not do a url update on the current tab
+    if (e.which === 1) {
+      chrome.tabs.update({ url: 'https://' + inLink })
+    }
+  })
 }
 
 debuglog('Popup: scrip end.')
