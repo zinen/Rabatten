24cd53cfcb25e742860964ab5fd13ece0ec6cb02
zinen <zinen@users.noreply.github.com>
2019-11-18 21:33:14 +0100

    Create utils:version

---
+++ utils/manifest-version.js
@@ -0,0 +1,21 @@
+var fs = require('fs')
+
+const packageVersion = process.env.npm_package_version
+console.log('Package version: ' + packageVersion)
+const file = 'src/manifest.json'
+
+function readWriteSync () {
+  let data = fs.readFileSync(file, 'utf-8')
+  data = JSON.parse(data)
+  console.log('Manifest version: ' + data.version)
+  if (packageVersion !== data.version) {
+    data.version = packageVersion
+    data = JSON.stringify(data, null, 2)
+    fs.writeFileSync(file, data, 'utf-8')
+    console.log('Write to manifest complete, data :' + packageVersion)
+  } else {
+    console.log('No need to update manifest version')
+  }
+}
+
+readWriteSync()
