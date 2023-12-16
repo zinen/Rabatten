d28ba3ed67feef7e81b920eeb31fc91e26a7bbea
zinen <9595798+zinen@users.noreply.github.com>
2023-12-16 21:00:09 +0100

    temp past double code

--- src/content.js
+++ src/content.js
@@ -1,6 +1,33 @@
-import { DiscountServices } from './common.js'
+// import { DiscountServices } from './common.js'
 console.log('Content: script is running!')
 let ignoreDomain
+
+const DiscountServices = { // eslint-disable-line no-unused-vars
+  forbrugsforeningen: {
+    name: 'Forbrugsforeningen',
+    arrayName: 'array_forbrugsforeningen',
+    databaseURL: 'https://cdn.jsdelivr.net/gh/zinen/rabatten-scraper@latest/dist/forbrugsforeningen.json',
+    homepage: 'http://forbrugsforeningen.dk'
+  },
+  logbuy: {
+    name: 'LogBuy',
+    arrayName: 'array_logbuy',
+    databaseURL: 'https://cdn.jsdelivr.net/gh/zinen/rabatten-scraper@latest/dist/logbuy.json',
+    homepage: 'http://logbuy.com'
+  },
+  coop: {
+    name: 'Coop partnerfordele',
+    arrayName: 'array_coop',
+    databaseURL: 'https://cdn.jsdelivr.net/gh/zinen/rabatten-scraper@latest/dist/coop.json',
+    homepage: 'https://partnerfordele.coop.dk/'
+  },
+  aeld: {
+    name: 'Ældre Sagen',
+    arrayName: 'array_aeld',
+    databaseURL: 'https://cdn.jsdelivr.net/gh/zinen/rabatten-scraper@latest/dist/aeld.json',
+    homepage: 'https://www.aeldresagen.dk/'
+  }
+}
 
 let tabDomainName = document.domain.split('.')
 if (tabDomainName[tabDomainName.length - 1] === 'uk') {
